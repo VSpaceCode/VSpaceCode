d8a03dc83f73be2d54f99daadce3174c5789102d
Steven Guh <stevenguh@gmail.com>
2020-06-24 18:48:58 -0700

    Move all command ids to constants

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import { commands, ExtensionContext, extensions } from 'vscode';
 import { configKeyBindings, configSettings } from './configuration';
-import { ConfigKey, extensionId, extensionQualifiedId, GlobalState, spaceCmdId } from './constants';
+import { CommandId, ConfigKey, extensionId, extensionQualifiedId, GlobalState } from './constants';
 import { checkVim, showUpdateMessage, showWelcomeMessage } from './messages';
 
 export async function activate(context: ExtensionContext) {
@@ -19,25 +19,25 @@ export async function activate(context: ExtensionContext) {
     const arg = { bindings: [extensionId, ConfigKey.Bindings], overrides: [extensionId, ConfigKey.Overrides], title: 'VSpaceCode' };
     const magitArg = { bindings: [extensionId, ConfigKey.RefBindings] };
     await Promise.all([
-        commands.executeCommand('whichkey.register', arg),
-        commands.executeCommand('whichkey.register', magitArg)
+        commands.executeCommand(CommandId.RegisterWhichKey, arg),
+        commands.executeCommand(CommandId.RegisterWhichKey, magitArg)
     ]);
 
-    context.subscriptions.push(commands.registerCommand(spaceCmdId, () => {
-        commands.executeCommand("whichkey.show", arg.bindings.join('.'));
+    context.subscriptions.push(commands.registerCommand(CommandId.ShowSpaceMenu, () => {
+        commands.executeCommand(CommandId.ShowWhichKey, arg.bindings.join('.'));
     }));
-    context.subscriptions.push(commands.registerCommand("vspacecode.showMagitRefMenu", () => {
-        commands.executeCommand("whichkey.show", magitArg.bindings.join('.'));
+    context.subscriptions.push(commands.registerCommand(CommandId.ShowMagitRefMenu, () => {
+        commands.executeCommand(CommandId.ShowWhichKey, magitArg.bindings.join('.'));
     }));
 
-    context.subscriptions.push(commands.registerCommand("vspacecode.configure", () => {
+    context.subscriptions.push(commands.registerCommand(CommandId.Configure, () => {
         configSettings();
         configKeyBindings(context);
     }));
-    context.subscriptions.push(commands.registerCommand("vspacecode.configureSettings", () => {
+    context.subscriptions.push(commands.registerCommand(CommandId.ConfigureSettings, () => {
         configSettings();
     }));
-    context.subscriptions.push(commands.registerCommand("vspacecode.configureKeyBindings", () => {
+    context.subscriptions.push(commands.registerCommand(CommandId.ConfigureKeybindings, () => {
         configKeyBindings(context);
     }));
 }
