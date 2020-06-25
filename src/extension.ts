1d8843de1e71c435f7dc300f24de7f8dd3a38b13
Steven Guh <stevenguh@gmail.com>
2020-06-24 17:57:58 -0700

    Add magit ref menu

--- src/extension.ts
+++ src/extension.ts
@@ -16,10 +16,17 @@ export async function activate(context: ExtensionContext) {
     }
 
     const arg = { bindings: [extensionId, ConfigKey.Bindings], overrides: [extensionId, ConfigKey.Overrides], title: 'VSpaceCode' };
-    await commands.executeCommand('whichkey.register', arg);
+    const magitArg = { bindings: [extensionId, ConfigKey.RefBindings] };
+    await Promise.all([
+        commands.executeCommand('whichkey.register', arg),
+        commands.executeCommand('whichkey.register', magitArg)
+    ]);
 
     context.subscriptions.push(commands.registerCommand(spaceCmdId, () => {
         commands.executeCommand("whichkey.show", arg.bindings.join('.'));
+    }));
+    context.subscriptions.push(commands.registerCommand("vspacecode.showMagitRefMenu", (_args: any[]) => {
+        commands.executeCommand("whichkey.show", magitArg.bindings.join('.'));
     }));
 }
 
