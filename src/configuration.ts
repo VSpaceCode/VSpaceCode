41bc22865d169a95873b05853f7df5eacca4ef1e
Steven Guh <stevenguh@gmail.com>
2020-06-25 21:08:09 -0700

    Use cmd to open keybindings instead of a hack

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,6 @@
 import { getNodeValue, Node, parseTree } from "jsonc-parser";
-import * as path from 'path';
-import { ConfigurationTarget, Range, window, workspace, WorkspaceEdit, commands } from "vscode";
+import { commands, ConfigurationTarget, Range, TextDocument, Uri, window, workspace, WorkspaceEdit } from "vscode";
 import { CommandId, VimConfigKey, vimExtensionId } from "./constants";
-import { logPath } from "./extension";
 import { KeyBinding } from "./keyBinding";
 import requiredBindings from './keybindings.json';
 import { VimKeyBinding } from "./vimKeyBinding";
@@ -26,60 +24,72 @@ export async function configSettings() {
 }
 
 export async function configKeyBindings() {
-    let keybindingsPath = path.join(logPath, '..', '..', '..', '..', 'User', 'keybindings.json');
-    try {
-        // This step is to create the keybindings.json if needed
-        await commands.executeCommand("workbench.action.openGlobalKeybindingsFile");
-        const doc = await workspace.openTextDocument(keybindingsPath);
-        const text = doc.getText();
-        // return undefined if text is an empty string
-        const node = parseTree(text) as Node | undefined;
-
-        const additionBindings = new Map<string, KeyBinding>();
-        for (const override of requiredBindings) {
-            const key = `${override.key}${override.command}`;
-            additionBindings.set(key, override);
+    await commands.executeCommand("workbench.action.openGlobalKeybindingsFile");
+    if (window.activeTextEditor) {
+        const document = window.activeTextEditor.document;
+        if (getFilename(document.uri) === 'keybindings.json') {
+            await editBindingsDoc(document);
         }
+    }
+}
 
-        if (node && node.type === 'array' && node.children) {
-            // Exclude the bindings that exists
-            for (let child of node.children) {
-                const v = getNodeValue(child) as KeyBinding;
-                const key = `${v.key}${v.command}`;
-                if (additionBindings.has(key)) {
-                    additionBindings.delete(key);
-                }
+function getFilename(uri: Uri) {
+    let pieces = uri.fsPath.split(/[/\\]/g);
+    if (pieces[pieces.length - 1].length === 0) {
+        pieces.pop();
+    }
+    if (pieces.length > 0) {
+        return pieces[pieces.length - 1];
+    }
+    return '';
+}
+
+async function editBindingsDoc(doc: TextDocument) {
+    const text = doc.getText();
+    // return undefined if text is an empty string
+    const node = parseTree(text) as Node | undefined;
+
+    const additionBindings = new Map<string, KeyBinding>();
+    for (const override of requiredBindings) {
+        const key = `${override.key}${override.command}`;
+        additionBindings.set(key, override);
+    }
+
+    if (node && node.type === 'array' && node.children) {
+        // Exclude the bindings that exists
+        for (let child of node.children) {
+            const v = getNodeValue(child) as KeyBinding;
+            const key = `${v.key}${v.command}`;
+            if (additionBindings.has(key)) {
+                additionBindings.delete(key);
             }
-
-            // TODO: wait for the support of array insert to preserved comment
-            // https://github.com/microsoft/node-jsonc-parser/pull/35
-            // let updatedText = text;
-            // let index = node.children.length;
-            // for (const binding of additionBindings.values()) {
-            //     const path = [index] as JSONPath;
-            //     const edits = modify(updatedText, path, binding, { formattingOptions: {} });
-            //     updatedText = applyEdits(updatedText, edits);
-            // }
         }
 
-        const rootArray = (node ? getNodeValue(node) : []) as KeyBinding[];
-        for (const binding of additionBindings.values()) {
-            rootArray.push(binding);
-        }
+        // TODO: wait for the support of array insert to preserved comment
+        // https://github.com/microsoft/node-jsonc-parser/pull/35
+        // let updatedText = text;
+        // let index = node.children.length;
+        // for (const binding of additionBindings.values()) {
+        //     const path = [index] as JSONPath;
+        //     const edits = modify(updatedText, path, binding, { formattingOptions: {} });
+        //     updatedText = applyEdits(updatedText, edits);
+        // }
+    }
 
-        if (rootArray.length > 0) {
-            const updatedText = JSON.stringify(rootArray, undefined, '\t');
+    const rootArray = (node ? getNodeValue(node) : []) as KeyBinding[];
+    for (const binding of additionBindings.values()) {
+        rootArray.push(binding);
+    }
 
-            const fullRange = new Range(
-                doc.positionAt(0),
-                doc.positionAt(text.length));
-            const edit = new WorkspaceEdit();
-            edit.replace(doc.uri, fullRange, updatedText);
-            await workspace.applyEdit(edit);
-            await doc.save();
-        }
-    } catch (e) {
-        console.error(e);
-        window.showErrorMessage('Failed to edit keybindings.json');
+    if (rootArray.length > 0) {
+        const updatedText = JSON.stringify(rootArray, undefined, '\t');
+
+        const fullRange = new Range(
+            doc.positionAt(0),
+            doc.positionAt(text.length));
+        const edit = new WorkspaceEdit();
+        edit.replace(doc.uri, fullRange, updatedText);
+        await workspace.applyEdit(edit);
+        await doc.save();
     }
 }
