4840dd425c377911b7f4ce87ca9d96e021def4a3
Steven Guh <stevenguh@gmail.com>
2020-08-26 21:21:38 -0700

    Implement platform guess for vscode-remote

---
+++ src/charCode.ts
@@ -0,0 +1,20 @@
+export const enum CharCode {
+    /**
+	 * The `/` character.
+	 */
+    Slash = 47,
+	/**
+	 * The `:` character.
+	 */
+    Colon = 58,
+
+    A = 65,
+    Z = 90,
+    a = 97,
+    z = 122,
+
+    /**
+     * The `\` character.
+     */
+    Backslash = 92,
+}
