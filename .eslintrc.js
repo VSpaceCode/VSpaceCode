672c40feb52a26d01e26d3466192506f1b834742
Steven Guh <3455662+stevenguh@users.noreply.github.com>
2022-04-03 12:22:31 -0700

    Add prettier configuration
    This is similar to https://github.com/VSpaceCode/vscode-which-key/commit/1a7f1e474ac6f28b63eb653e8f9683d05842e0b4

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,24 @@
 module.exports = {
     root: true,
     env: {
-        "es6": true,
-        "node": true,
-        "mocha": true,
+        es6: true,
+        node: true,
+        mocha: true,
     },
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
     ],
     rules: {
-        'semi': [2, "always"],
-        'eol-last': 'error',
-        '@typescript-eslint/no-unused-vars': 0,
-        '@typescript-eslint/no-var-requires': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 0,
-        '@typescript-eslint/no-non-null-assertion': 0,
-    }
+        semi: [2, "always"],
+        "eol-last": "error",
+        "@typescript-eslint/no-unused-vars": 0,
+        "@typescript-eslint/no-var-requires": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+    },
 };
