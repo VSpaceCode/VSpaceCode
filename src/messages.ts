32d87111c5b95b5f0c3deb6fb54e1ff9e64c4992
Steven Guh <3455662+stevenguh@users.noreply.github.com>
2022-11-22 21:09:09 -0800

    Replace initial notification with walkthroughs (#291)
    This also bump the required version to v1.67.
     - v1.67+ is required to use to tab API (https://github.com/microsoft/vscode/commit/aa69f3d7623c464aba726d12ea0d83428f43e8b9)
     - v1.57+ is required to use the walkthroughs API

--- src/messages.ts
+++ src/messages.ts
@@ -1,28 +1,5 @@
-import { commands, env, Uri, window } from "vscode";
-import { CommandId, manualInstallUrl } from "./constants";
+import { env, Uri, window } from "vscode";
 import { ComparisonResult, Version } from "./version";
-
-const enum WelcomeSelection {
-    Auto = "Automatically",
-    Manually = "Manually",
-}
-
-export async function showWelcomeMessage() {
-    const selection = await window.showInformationMessage(
-        `Welcome to VSpaceCode. Some configurations are needed to get started. Choose ${WelcomeSelection.Auto} to merge necessary configurations to your user's settings.json and keybindings.json. However, formatting in those file may be lost.`,
-        WelcomeSelection.Manually,
-        WelcomeSelection.Auto
-    );
-
-    switch (selection) {
-        case WelcomeSelection.Manually:
-            await env.openExternal(Uri.parse(manualInstallUrl));
-            break;
-        case WelcomeSelection.Auto:
-            await commands.executeCommand(CommandId.Configure);
-            break;
-    }
-}
 
 export async function showUpdateMessage(cur: string, prev: string) {
     if (Version.compare(cur, prev) === ComparisonResult.Newer) {
